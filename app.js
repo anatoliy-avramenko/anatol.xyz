cfa2d6a9f76739c84920a1d77743ac706c1b820c
Anatoliy Avramenko <anatoliy.avramenko@xenial.com>
2020-12-19 22:16:16 +0200

    Add /teapot endpoint

--- app.js
+++ app.js
@@ -3,7 +3,13 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('hello there, user!');	
+  res.send('hello there, user!');
+});
+
+app.get('teapot', (req, res) => {
+  res.set('Z-full-stack', '4life');
+  res.status(418);
+  res.send('I prefer coffee');
 });
 
 app.listen(port, () => console.log(`example app listening on port ${port}`));
